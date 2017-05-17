7755325d3b36e7bc511dd486fae2345fe6c73263
李宝 <libao@libaos-MacBook-Pro.local>
2017-05-17 22:34:28 +0800

    dispaly value on page

--- message/views.py
+++ message/views.py
@@ -7,15 +7,21 @@ from .models import UserMessage
 # Create your views here.
 
 def getform(request):
-    if request.method == "POST":
-        name = request.POST.get('name','')
-        message = request.POST.get('message','')
-        address = request.POST.get('address','')
-        email = request.POST.get('email','')
-        user_message = UserMessage()
-        user_message.name = name
-        user_message.message = message
-        user_message.address = address
-        user_message.email = email
-        user_message.save()
-    return render(request, "message_form.html")
+    message = None
+    all_messages = UserMessage.objects.filter(name='李宝')
+    if all_messages:
+        message = all_messages[0]
+    # if request.method == "POST":
+    #     name = request.POST.get('name','')
+    #     message = request.POST.get('message','')
+    #     address = request.POST.get('address','')
+    #     email = request.POST.get('email','')
+    #     user_message = UserMessage()
+    #     user_message.name = name
+    #     user_message.message = message
+    #     user_message.address = address
+    #     user_message.email = email
+    #     user_message.save()
+    return render(request, "message_form.html", {
+        "my_message":message
+    })
